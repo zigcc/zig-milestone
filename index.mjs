c413c6def0e63cc3b981841a76225db77c87be27
jiacai2050 <dev@liujiacai.net>
2024-03-28 21:14:06 +0800

    first init

---
+++ index.mjs
@@ -0,0 +1,55 @@
+import { createClient } from "@libsql/client";
+
+const client = createClient({
+  url: process.env.URL ?? "file:local.db",
+  authToken: process.env.TURSO_TOKEN,
+});
+
+async function fetch_milestone_histories() {
+  const rs = await client.execute(
+    "select id from milestones where state = 'open'"
+  );
+  const now = Date.now();
+  const sqls = [];
+  for (const row of rs.rows) {
+    const mid = row['id'];
+    const resp = await fetch(`https://api.github.com/repos/ziglang/zig/milestones/${mid}`);
+    const m = await resp.json();
+
+    try {
+      const r = await client.execute({
+        sql: "INSERT INTO milestone_histories VALUES (:mid, :created_at, :open_issues, :closed_issues)",
+        args: { mid: mid,
+                open_issues: m['open_issues'],
+                closed_issues: m['closed_issues'],
+                created_at: now,
+              },
+      });
+      console.log(milestones);
+    } catch (e) {
+      console.error(mid, e);
+    }
+  }
+
+}
+
+await fetch_milestone_histories();
+
+async function fetch_milestones() {
+  const resp = await fetch('https://api.github.com/repos/ziglang/zig/milestones');
+  const milestones = await resp.json();
+  for(const m of milestones) {
+    const r = await client.execute({
+      sql: "INSERT INTO milestones VALUES (:id, :created_at, :updated_at, :state, :title, :description)",
+      args: { id: m['number'],
+              created_at: m['created_at'],
+              updated_at: m['updated_at'],
+              state: m['state'],
+              title: m['title'],
+              description: m['description'],
+            },
+    });
+    console.log(`insert ret ${JSON.stringify(r)}`);
+  }
+
+}
